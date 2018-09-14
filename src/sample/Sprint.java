c7d1b30227d558e10359f5d2b6e585529855dced
lucas <lucasluizpelepenko@gmail.com>
2018-09-14 12:30:42 -0300

    #telasprintCompleta

--- src/sample/Sprint.java
+++ src/sample/Sprint.java
@@ -2,17 +2,29 @@ package sample;
 
 import javafx.beans.property.SimpleStringProperty;
 
+
 public class Sprint {
 
     public SimpleStringProperty nome = new SimpleStringProperty();
-    public SimpleStringProperty Status = new SimpleStringProperty();
+    public SimpleStringProperty datainicio = new SimpleStringProperty();
+    public SimpleStringProperty datafim = new SimpleStringProperty();
+    public SimpleStringProperty status = new SimpleStringProperty();
 
-    public String getnome(){
+
+    public String getNome(){
         return nome.get();
     }
 
-    public String Status(){
-        return Status.get();
+    public String getDatainicio(){
+        return datainicio.get();
+    }
+
+    public String getDatafim(){
+        return datafim.get();
+    }
+
+    public String getStatus(){
+        return status.get();
     }
 
 }
--- src/sample/SprintController.java
+++ src/sample/SprintController.java
@@ -1,56 +1,161 @@
 package sample;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import javax.swing.table.TableColumn;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.io.IOException;
+public class SprintController implements Initializable {
 
-public class SprintController {
+    // DECLARAÇÃO DOS COMPONENTES DA TELA
+    @FXML private Button AdicionarBT;
+    @FXML private Button ModificarBT;
+    @FXML private Button RemoverBT;
+    @FXML private Button NovoBT;
 
-    //botton
-    @FXML private Button adicionarBT;
-    @FXML private Button modificarBT;
 
-    //label
-    @FXML private TextField nomeLB;
-    @FXML private TextField StatusDB;
+    @FXML private TextField nomeTF;
+    @FXML private TextField datainicioTF;
+    @FXML private TextField datafimTF;
+    @FXML private TextField statusTF;
 
-    //tabela
+
     @FXML private TableView<Sprint> tableSprint;
-    @FXML private TableColumn tableSprintNome;
-    @FXML private TableColumn tableSprintStatus;
+    @FXML private TableColumn nomeCL;
+    @FXML private TableColumn datainicioCL;
+    @FXML private TableColumn datafimCL;
+    @FXML private TableColumn statusCL;
     ObservableList<Sprint> sprints;
 
-    private int pocisao;
+    private int posicionPersonaEnTabla;
 
-    @FXML
-    public void novo(MouseEvent event) throws IOException{
-        Sprint sprint = new Sprint();
-        nomeLB.setText("TESTE");
-        StatusDB.setText("TESTE");
-        sprints.add(sprint);
+    @FXML private void Novo(ActionEvent event) {
+        nomeTF.setText("");
+        datainicioTF.setText("");
+        datafimTF.setText("");
+        statusTF.setText("");
+        ModificarBT.setDisable(true);
+        RemoverBT.setDisable(true);
+        AdicionarBT.setDisable(false);
     }
 
-    private void handleButtonAction(ActionEvent event){
-        System.out.println("Hello World");
+    @FXML private void Adicionar(ActionEvent event) {
+        Sprint minhaSprint = new Sprint();
+        minhaSprint.nome.set(nomeTF.getText());
+        minhaSprint.datainicio.set(datainicioTF.getText());
+        minhaSprint.datafim.set(datafimTF.getText());
+        minhaSprint.status.set(statusTF.getText());
+        sprints.add(minhaSprint);
     }
 
-    public void handleVoltar(MouseEvent event) throws IOException {
+    @FXML private void Modificar(ActionEvent event) {
+        Sprint persona = new Sprint();
+        persona.nome.set(nomeTF.getText());
+        persona.datainicio.set(datainicioTF.getText());
+        persona.datafim.set(datafimTF.getText());
+        persona.status.set(statusTF.getText());
+        sprints.set(posicionPersonaEnTabla, persona);
+    }
+
+    @FXML private void Remover(ActionEvent event) {
+        sprints.remove(posicionPersonaEnTabla);
+    }
+
+    //detectar mudanças para apos clicar em algum dado carregar os mesmo nos TF
+    private final ListChangeListener<Sprint> selectorTablaPersonas =
+            new ListChangeListener<Sprint>() {
+                @Override
+                public void onChanged(ListChangeListener.Change<? extends Sprint> c) {
+                    carregadadosTF();
+                }
+            };
+
+
+    public Sprint getTablaPersonasSeleccionada() {
+        if (tableSprint != null) {
+            List<Sprint> tabla = tableSprint.getSelectionModel().getSelectedItems();
+            if (tabla.size() == 1) {
+                final Sprint competicionSeleccionada = tabla.get(0);
+                return competicionSeleccionada;
+            }
+        }
+        return null;
+    }
+
+    private void carregadadosTF() {
+        final Sprint persona = getTablaPersonasSeleccionada();
+        posicionPersonaEnTabla = sprints.indexOf(persona);
+
+        if (persona != null) {
+
+            nomeTF.setText(persona.getNome());
+            datainicioTF.setText(persona.getDatainicio());
+            datafimTF.setText(persona.getDatafim());
+            statusTF.setText(persona.getStatus());
+
+
+            ModificarBT.setDisable(false);
+            RemoverBT.setDisable(false);
+            AdicionarBT.setDisable(true);
+
+        }
+    }
+
+    private void inicializarTablaPersonas() {
+        nomeCL.setCellValueFactory(new PropertyValueFactory<Sprint, String>("nome"));
+        datainicioCL.setCellValueFactory(new PropertyValueFactory<Sprint, String>("datainicio"));
+        datafimCL.setCellValueFactory(new PropertyValueFactory<Sprint, String>("datafim"));
+        statusCL.setCellValueFactory(new PropertyValueFactory<Sprint, String>("status"));
+
+        sprints = FXCollections.observableArrayList();
+        tableSprint.setItems(sprints);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        //iniciar tabela
+        this.inicializarTablaPersonas();
+
+        ModificarBT.setDisable(true);
+        RemoverBT.setDisable(true);
+
+        final ObservableList<Sprint> tablaPersonaSel = tableSprint.getSelectionModel().getSelectedItems();
+        tablaPersonaSel.addListener(selectorTablaPersonas);
+
+        //Exemplo para carregar tabela
+        //for (int i = 0; i < 5; i++) {
+        //    Sprint p1 = new Sprint();
+        //    p1.nome.set("1.0 SP " + i);
+        //    p1.datainicio.set("13/09/2018");
+        //    p1.datafim.set("28/09/2018");
+        //    p1.status.set("Em progresso");
+        //    sprints.add(p1);
+        //}
+    }
+
+    public void handleVoltarmenu(MouseEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(root);
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
-
 }
