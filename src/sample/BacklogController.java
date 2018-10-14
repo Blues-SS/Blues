f5e3996b474265313892b0578100e0537833fddf
Lucas Pelepenko <lucasluizpelepenko@gmail.com>
2018-10-13 21:03:53 -0300

    #telaBackLog

--- src/sample/BacklogController.java
+++ src/sample/BacklogController.java
@@ -2,11 +2,12 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
@@ -28,8 +29,13 @@ public class BacklogController {
 
     @FXML
     private Button cancelarBT;
+    @FXML
+    private AnchorPane mainSprint;
+    @FXML
+    private AnchorPane main;
 
-
+    private double xOffset = 0;
+    private double yOffset = 0;
     int i = 0;
     private static SprintDAO sprintDAO;
     private static Conexao conexao = new Conexao();
@@ -43,8 +49,10 @@ public class BacklogController {
         historia = FXCollections.observableArrayList();
     }
 
+
+
     public void handleNovaHistoria(MouseEvent event) throws IOException {
-        AnchorPane novaHistoria = FXMLLoader.load(getClass().getResource("Historia.fxml"));
+        AnchorPane novaHistoria = FXMLLoader.load(getClass().getResource("historiaBacklog.fxml"));
         novaHistoria.setId("Hist" + i);
 
         //-----------------------
@@ -60,28 +68,6 @@ public class BacklogController {
         historiaDAO.setStatus("TODO");
         this.sprintDAO.getHistorias().add(historiaDAO);
 
-        /*novaHistoria.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if (event.getScreenX() >= 754 && event.getScreenX() < 1168) {
-                    if (!doing.getChildren().contains(novaHistoria)) { // se já não estiver na pane DOING, adiciona
-                        doing.getChildren().add(novaHistoria);
-                        atualizaStatusHistoria(novaHistoria, "DOING");
-                    }
-                } else if (event.getScreenX() >= 1168) {
-                    if (!done.getChildren().contains(novaHistoria)) { // se já não estiver na pane DONE, adiciona
-                        done.getChildren().add(novaHistoria);
-                        atualizaStatusHistoria(novaHistoria, "DONE");
-                    }
-                } else if (event.getScreenX() < 752 && event.getScreenX() > 0) {
-                    if (!toDo.getChildren().contains(novaHistoria)) { // se já não estiver na pane TO DO, adiciona
-                        toDo.getChildren().add(novaHistoria);
-                        atualizaStatusHistoria(novaHistoria, "TODO");
-                    }
-                }
-            }
-        });*/
-
         ComboBox histPts = (ComboBox) novaHistoria.lookup("#histPts");
         histPts.getItems().addAll(1, 2, 3, 5, 8, 13);
         ComboBox valueBus = (ComboBox) novaHistoria.lookup("#valueBus");
@@ -96,28 +82,87 @@ public class BacklogController {
                     pts != null ? Integer.valueOf(pts.toString()) : null,
                     null);
         });
-        valueBus.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            String text = tituloHist.getText();
-            Object pts = histPts.getSelectionModel().getSelectedItem();
-            atualizaDadosHistoria(novaHistoria,
-                    text,
-                    newValue != null ? Integer.valueOf(newValue.toString()) : null,
-                    pts != null ? Integer.valueOf(pts.toString()) : null,
-                    null);
-        });
-        histPts.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            String text = tituloHist.getText();
-            Object business = valueBus.getSelectionModel().getSelectedItem();
-            atualizaDadosHistoria(novaHistoria,
-                    text,
-                    business != null ? Integer.valueOf(business.toString()) : null,
-                    newValue != null ? Integer.valueOf(newValue.toString()) : null,
-                    null);
-        });
+
         javafx.scene.control.Button histBtn = (javafx.scene.control.Button) novaHistoria.lookup("#histBtn");
+
+        //abrir informaçoes
+        histBtn.setOnAction(actionEvent -> {
+            AnchorPane infoTela = null;
+            try {
+                infoTela = FXMLLoader.load(getClass().getResource("newhistBacklog.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ComboBox pontosCB = (ComboBox) infoTela.lookup("#pontosCB");
+            pontosCB.getItems().addAll(1, 2, 3, 5, 8, 13);
+            ComboBox valueCB = (ComboBox) infoTela.lookup("#valueCB");
+            valueCB.getItems().addAll(1000, 3000, 5000);
+            javafx.scene.control.TextField tituloTF = (javafx.scene.control.TextField) infoTela.lookup("#tituloTF");
+
+            tituloTF.textProperty().addListener((observable, oldValue, newValue) -> {
+                Object business = valueCB.getSelectionModel().getSelectedItem();
+                Object pts = pontosCB.getSelectionModel().getSelectedItem();
+                atualizaDadosHistoria(novaHistoria,
+                        newValue,
+                        business != null ? Integer.valueOf(business.toString()) : null,
+                        pts != null ? Integer.valueOf(pts.toString()) : null,
+                        null);
+            });
+            valueCB.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                String text = tituloTF.getText();
+                Object pts = pontosCB.getSelectionModel().getSelectedItem();
+                atualizaDadosHistoria(novaHistoria,
+                        text,
+                        newValue != null ? Integer.valueOf(newValue.toString()) : null,
+                        pts != null ? Integer.valueOf(pts.toString()) : null,
+                        null);
+            });
+            pontosCB.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                String text = tituloTF.getText();
+                Object business = valueCB.getSelectionModel().getSelectedItem();
+                atualizaDadosHistoria(novaHistoria,
+                        text,
+                        business != null ? Integer.valueOf(business.toString()) : null,
+                        newValue != null ? Integer.valueOf(newValue.toString()) : null,
+                        null);
+            });
+
+
+
+            javafx.scene.control.TextArea descrHist = (javafx.scene.control.TextArea) infoTela.lookup("#descrHist");
+            javafx.scene.control.Button infoSalvar = (javafx.scene.control.Button) infoTela.lookup("#infoSalvar");
+            infoSalvar.setOnAction(actionEvent2 -> {
+                String text = tituloHist.getText();
+                Object business = valueBus.getSelectionModel().getSelectedItem();
+                Object pts = histPts.getSelectionModel().getSelectedItem();
+                atualizaDadosHistoria(novaHistoria,
+                        text,
+                        business != null ? Integer.valueOf(business.toString()) : null,
+                        pts != null ? Integer.valueOf(pts.toString()) : null,
+                        descrHist.getText());
+                mainSprint.getChildren().remove(mainSprint.lookup("#newhistBacklog"));
+                mainSprint.setDisable(true);
+                mainSprint.setVisible(false);
+            });
+
+            mainSprint.setStyle("-fx-background-color: rgba(128, 128, 128, 0.4)");
+            mainSprint.setDisable(false);
+            mainSprint.setVisible(true);
+            mainSprint.getChildren().addAll(infoTela);
+
+            javafx.scene.control.Button infoCancel = (javafx.scene.control.Button) infoTela.lookup("#infoCancel");
+            infoCancel.setOnAction(actionEvent3 -> {
+                mainSprint.getChildren().remove(mainSprint.lookup("#newhistBacklog"));
+                mainSprint.setDisable(true);
+                mainSprint.setVisible(false);
+            });
+        });
+
         toDo.getChildren().add(novaHistoria);
         i++;
     }
+
     public void atualizaDadosHistoria(AnchorPane novaHistoria, String text, Integer business, Integer pts, String descr) {
         HistoriaDAO historiaDAO = new HistoriaDAO();
         AtomicReference<Integer> index = new AtomicReference<>();
@@ -146,6 +191,32 @@ public class BacklogController {
     }
 
     @FXML
+    public void voltar(MouseEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Para deixar a tela draggable
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void cancelar(InputEvent e) {
         //fechar tela somente
         final Node source = (Node) e.getSource();
